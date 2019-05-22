b25090d923237994380c278255a3b78758cb603b
paulronla <50727954+paulronla@users.noreply.github.com>
2019-05-21 23:57:44 -0500

    1) Added ability to download a range of pages every WAIT_TIME ms. 2) Moved URL as a constant. 3) Added comments.

--- teochew_dict_scraper.js
+++ teochew_dict_scraper.js
@@ -2,20 +2,57 @@
 const http = require('http');
 const fs = require('fs');
 const WRITE_PATH = '';
-let page = 0;
+const START_PAGE = 57;
+const END_PAGE = 62;
+const WAIT_TIME = 10000;
+const URL = `http://www.czyzd.com/ajax/list?page=0&keyword=&pinyin=&chaoyin=&bushou=&bihua=0`;
 
-http.get('http://www.czyzd.com/ajax/list?page='+page+'&keyword=&pinyin=&chaoyin=&bushou=&bihua=0', function (res) {
-    const sb_text = [];
+const getWebPage = genGetWebPage(URL);
+getWebPageWithDelay(START_PAGE);
 
-    res.setEncoding('utf8');
-    res.on('data', chunk => sb_text.push(chunk));
-    res.on('end', () => writeToFile(sb_text, WRITE_PATH, page));
-});
+/*
+genGetWebPage(URL)
+URL is a string
+returns a function
 
-function writeToFile (stringBuilder, writePath, fileName) {
+The returned function creates a closure around genGetWebPage which keeps alive frontURL and endURL,
+so that it only needs to be processed once per script execution.
+*/
+function genGetWebPage(URL) {
+    let [frontURL, endURL] = URL.split('page=0');
+    frontURL += 'page=';
+
+    return function (pageNum) {
+        console.log('Downloading page ' +pageNum + '\n');
+
+        http.get(frontURL + pageNum + endURL, function (res) {
+            const sb_text = [];
+
+            res.setEncoding('utf8');
+            res.on('data', chunk => sb_text.push(chunk));
+            res.on('end', () => writeToFile(sb_text.join(''), WRITE_PATH, pageNum));
+        });
+    }
+}
+
+/*
+getWebPageWithDelay(pageNum)
+pageNum is a number
+returns void
+
+Calls getWebPage(pageNum) every WAIT_TIME ms from START_PAGE to END_PAGE, inclusive.
+*/
+function getWebPageWithDelay(pageNum) {
+    getWebPage(pageNum);
+
+    if (pageNum < END_PAGE)
+        setTimeout(() => getWebPageWithDelay(pageNum+1), WAIT_TIME);
+}
+
+function writeToFile (text, writePath, fileName) {
     const file = WRITE_PATH + fileName + '.html';
-    fs.writeFile(file, stringBuilder.join(''), (err) => {
-        if (err) throw err; 
-        console.log(file + ' saved.');
+
+    fs.writeFile(file, text, () => {
+        console.log(file + ' saved.\n');
     });
 }
